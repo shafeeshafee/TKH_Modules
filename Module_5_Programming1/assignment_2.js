68a683b208aa8379f39646f81aa23a66bf5cc52b
Shafee <shafeelinks@gmail.com>
2020-08-02 02:24:59 -0400

    number to letter grade app completed

---
+++ Module_5_Programming1/assignment_2.js
@@ -0,0 +1,38 @@
+const grade = parseInt(prompt("Enter the number grade you received: "));
+
+switch (true) {
+  case grade > 95:
+    letterGrade = "A+";
+    console.log(letterGrade);
+    break;
+  case grade > 88:
+    letterGrade = "A";
+    console.log(letterGrade);
+    break;
+  case grade > 84:
+    letterGrade = "B+";
+    console.log(letterGrade);
+    break;
+  case grade > 76:
+    letterGrade = "C+";
+    console.log(letterGrade);
+    break;
+  case grade > 70:
+    letterGrade = "C";
+    console.log(letterGrade);
+    break;
+  case grade > 67:
+    letterGrade = "D+";
+    console.log(letterGrade);
+    break;
+  case grade > 64:
+   letterGrade = "D";
+    console.log(letterGrade);
+    break;
+  case grade < 64:
+    letterGrade = "F";
+    console.log(letterGrade);
+    break;
+  default:
+    console.log("Enter a number grade.");
+}
