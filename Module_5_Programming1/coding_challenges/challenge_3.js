1b3a8f3eb9da591f414d2ba142beaef10d2db882
Shafee <shafeelinks@gmail.com>
2020-08-02 03:37:21 -0400

    challenge 3 solved

---
+++ Module_5_Programming1/coding_challenges/challenge_3.js
@@ -0,0 +1,12 @@
+// change every letter 's' into '$' in these song lyrics
+
+const lyrics = `Wipe the sweat off my dome, spit the phlegm on the streets Suede Timbs on my feets makes my cipher complete Whether cruising in a Sikh's cab, or Montero Jeep I can't call it, the beats make me falling asleep I keep falling, but never falling six feet deep I'm out for presidents to represent me, I'm out for presidents to represent me, I'm out for dead presidents to represent me,`
+const arrS = lyrics.split(" ");
+console.log(arrS);
+
+for (i = 0; i < arrS.length; i++) {
+  arrS[i] = arrS[i].replace('s', '$');
+  arrS.toString()
+}
+
+console.log(arrS.join(' '));
