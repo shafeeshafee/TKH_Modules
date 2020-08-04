45939a6eaf3a2b34110beab2facc9994540e6f45
Shafee <shafeelinks@gmail.com>
2020-08-04 08:51:07 -0400

    tidied up some of the code

--- Module_5_Programming1/coding_challenges/challenge_1.js
+++ Module_5_Programming1/coding_challenges/challenge_1.js
@@ -2,17 +2,15 @@
 
 // seperate first and last names into their own arrays
 
-let names = ["Westly Snipes", "Nicholas Cage", "Nasir Jones", "Sean Carter", "Sean Combs", "Michael Jordan", "Patrick Ewing"]
+let names = ["Westly Snipes", "Nicholas Cage", "Nasir Jones", "Sean Carter", "Sean Combs", "Michael Jordan", "Patrick Ewing"];
 
 let firstName = [];
 let lastName = [];
 
 for (i = 0; i < names.length; i++) {
-  
- let nameSplit = names[i].split(" ");
- firstName.push(nameSplit[0]);
- lastName.push(nameSplit[1]);
- 
+    let nameSplit = names[i].split(" ");
+    firstName.push(nameSplit[0]);
+    lastName.push(nameSplit[1]);
 }
 
 console.log(firstName);
