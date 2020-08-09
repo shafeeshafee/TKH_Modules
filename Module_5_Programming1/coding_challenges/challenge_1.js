1a4401e085cf26e9a297482f924cf55d7ede36d8
Shafee <shafeelinks@gmail.com>
2020-08-09 18:12:06 -0400

    added description to console logging first and last name

--- Module_5_Programming1/coding_challenges/challenge_1.js
+++ Module_5_Programming1/coding_challenges/challenge_1.js
@@ -2,16 +2,24 @@
 
 // seperate first and last names into their own arrays
 
-let names = ["Westly Snipes", "Nicholas Cage", "Nasir Jones", "Sean Carter", "Sean Combs", "Michael Jordan", "Patrick Ewing"];
+let names = [
+	"Westly Snipes",
+	"Nicholas Cage",
+	"Nasir Jones",
+	"Sean Carter",
+	"Sean Combs",
+	"Michael Jordan",
+	"Patrick Ewing",
+];
 
 let firstName = [];
 let lastName = [];
 
 for (i = 0; i < names.length; i++) {
-    let nameSplit = names[i].split(" ");
-    firstName.push(nameSplit[0]);
-    lastName.push(nameSplit[1]);
+	let nameSplit = names[i].split(" ");
+	firstName.push(nameSplit[0]);
+	lastName.push(nameSplit[1]);
 }
 
-console.log(firstName);
-console.log(lastName);
+console.log("These are the first names: ", firstName);
+console.log("These are the last names: ", lastName);
