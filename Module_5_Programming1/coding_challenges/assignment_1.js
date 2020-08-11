a8bb4d5250429a9fd4c87a24888fcf54506e19c5
Shafee <shafeelinks@gmail.com>
2020-08-11 06:38:51 -0400

    cleaned up lines a little

--- Module_5_Programming1/coding_challenges/assignment_1.js
+++ Module_5_Programming1/coding_challenges/assignment_1.js
@@ -8,20 +8,16 @@
 let weeklyIncome = parseInt(prompt("How much is your weekly income?"));
 let food = parseInt(prompt("How much do you pay for food weekly?"));
 let housing = parseInt(prompt("Anually, how much do you pay for housing?"));
-let transportation = parseInt(
-  prompt("How much do you pay for transportation weekly?")
-);
-let otherCost = parseInt(
-  prompt("How much do you spend on other things weekly?")
-);
+let transportation = parseInt(prompt("How much do you pay for transportation weekly?"));
+let otherCost = parseInt(prompt("How much do you spend on other things weekly?"));
 let goal = parseInt(prompt("How much would you like to save in a year?"));
 
 // step 2 - how much to save a week
 let saveGoal = goal / 52;
 console.log(
-  "With your weekly salary, you would have to save $" +
-    saveGoal.toFixed(2) +
-    " every week to cover your goal."
+	"With your weekly salary, you would have to save $" +
+		saveGoal.toFixed(2) +
+		" every week to cover your goal."
 );
 
 // step 3 - total weekly costs
@@ -32,10 +28,10 @@ console.log("End of the week, you're left with: $" + amountLeft.toFixed(2));
 
 // step 4 - right track or need to save?
 if (amountLeft > saveGoal) {
-  console.log("You're on track!");
+	console.log("You're on track!");
 } else {
-  console.log(
-    "You're not on track. Currently, you need to save this much weekly: $" +
-      (saveGoal.toFixed(2) - amountLeft.toFixed(2))
-  );
+	console.log(
+		"You're not on track. Currently, you need to save this much weekly: $" +
+			(saveGoal.toFixed(2) - amountLeft.toFixed(2))
+	);
 }
