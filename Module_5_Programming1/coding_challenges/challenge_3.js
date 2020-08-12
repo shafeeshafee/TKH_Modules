125b636b0838cc1b9a962d2fdaa8d2eda9ce410b
Shafee <shafeelinks@gmail.com>
2020-08-11 21:23:36 -0400

    simplified regex replace method

--- Module_5_Programming1/coding_challenges/challenge_3.js
+++ Module_5_Programming1/coding_challenges/challenge_3.js
@@ -5,11 +5,11 @@
 const lyrics = `Wipe the sweat off my dome, spit the phlegm on the streets Suede Timbs on my feets makes my cipher complete Whether cruising in a Sikh's cab, or Montero Jeep I can't call it, the beats make me falling asleep I keep falling, but never falling six feet deep I'm out for presidents to represent me, I'm out for presidents to represent me, I'm out for dead presidents to represent me,`;
 const arrS = lyrics.split(" ");
 
-//replace s and S with regex global values
+// replace s and S with regex global values
 for (i = 0; i < arrS.length; i++) {
-  arrS[i] = arrS[i].replace(/s/g, "$").replace(/S/g, "$");
-  arrS.toString();
+	arrS[i] = arrS[i].replace(/s|S/gi, "$");
+	arrS.toString();
 }
 
-//log instance of lyrics
+// log instance of lyrics
 console.log(arrS.join(" "));
